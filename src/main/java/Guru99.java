71fc5414d0d34c8b646412b0fb3e976037bcd199
sri9dasari <sri9dasari@gmail.com>
2017-06-20 21:36:52 +0530

    pom

--- src/main/java/Guru99.java
+++ src/main/java/Guru99.java
@@ -2,16 +2,23 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class Guru99 {
-	public static void main(String[] args) {
-		FirefoxDriver driver = new FirefoxDriver();
+	FirefoxDriver driver;
+
+	@BeforeTest
+	public void setup() {
+		driver = new FirefoxDriver();
 		driver.get("http://demo.guru99.com/v4/");
 		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.findElement(By.xpath("/html/body/form/table/tbody/tr[1]/td[2]/input")).sendKeys("mngr79115");
 		driver.findElement(By.xpath("/html/body/form/table/tbody/tr[2]/td[2]/input")).sendKeys("juzamus");
 		driver.findElement(By.xpath("/html/body/form/table/tbody/tr[3]/td[2]/input[1]")).click();
+
 		driver.findElement(By.xpath("/html/body/div[2]/div/ul/li[2]/a")).click();
 		driver.findElement(By.xpath("/html/body/table/tbody/tr/td/table/tbody/tr[4]/td[2]/input")).sendKeys("Syam D");
 		driver.findElement(By.xpath("/html/body/table/tbody/tr/td/table/tbody/tr[5]/td[2]/input[1]")).click();
@@ -26,15 +33,59 @@ public class Guru99 {
 		driver.findElement(By.xpath("/html/body/table/tbody/tr/td/table/tbody/tr[11]/td[2]/input"))
 				.sendKeys("1234567890");
 		driver.findElement(By.xpath("/html/body/table/tbody/tr/td/table/tbody/tr[12]/td[2]/input"))
-				.sendKeys("abcdefty@gmail.com");
+				.sendKeys("abcdegggy@gmail.com");
 		driver.findElement(By.xpath("/html/body/table/tbody/tr/td/table/tbody/tr[13]/td[2]/input")).sendKeys("vgfjy");
 		driver.findElement(By.xpath("/html/body/table/tbody/tr/td/table/tbody/tr[14]/td[2]/input[1]")).click();
-		String name = driver.findElement(By.xpath("")).getText();
-		if (name.equals("Syam D")) {
-			System.out.println("Customer name matched");
-		} else {
-			System.out.println("customer name not matched");
-		}
+	}
+
+	@Test(groups={"positive","functional","customerCreation","name"},priority=1)
+	public void testCustomerName() {
+
+		String name = driver.findElement(By.xpath(".//*[@id='customer']/tbody/tr[5]/td[2]")).getText();
+		Assert.assertEquals(name, "Syam D");
+	}
+
+	@Test(groups={"positive","functional","customerCreation","address"},priority=5)
+	public void testCustomerAddress() {
+
+		String address = driver.findElement(By.xpath(".//*[@id='customer']/tbody/tr[8]/td[2]")).getText();
+		Assert.assertEquals(address, "hsrlllayout");
+	}
+
+	@Test(groups={"positive","functional","customerCreation","city"},priority=2)
+	public void testCustomerCity() {
+
+		String city = driver.findElement(By.xpath(".//*[@id='customer']/tbody/tr[9]/td[2]")).getText();
+		Assert.assertEquals(city, "bangalore");
+	}
+
+	@Test(groups={"positive","functional","customerCreation","state"},priority=4)
+	public void testCustomerState() {
+
+		String state = driver.findElement(By.xpath(".//*[@id='customer']/tbody/tr[10]/td[2]")).getText();
+		Assert.assertEquals(state, "karnataka");
+	}
+
+	@Test(groups={"positive","functional","customerCreation","pin"},enabled=false)
+	public void testCustomerpin() {
+
+		String pin = driver.findElement(By.xpath(".//*[@id='customer']/tbody/tr[11]/td[2]")).getText();
+		Assert.assertEquals(pin, "569934");
+	}
+
+	@Test(groups={"positive","functional","customerCreation","mobileNo"},priority=6,invocationCount=3)
+	public void testCustomerMobileNo() {
+
+		String mobileno = driver.findElement(By.xpath(".//*[@id='customer']/tbody/tr[12]/td[2]")).getText();
+		Assert.assertEquals(mobileno, "1234567890");
+	}
+
+	@Test(groups={"positive","functional","customerCreation","emailId"},priority=3)
+	public void testCustomerEmailId() {
+
+		String emailid = driver.findElement(By.xpath(".//*[@id='customer']/tbody/tr[13]/td[2]")).getText();
+		Assert.assertEquals(emailid, "abcdettty@gmail.com");
+
 	}
 
 }
